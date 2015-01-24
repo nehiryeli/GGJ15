7cf90c3872ada3284c4840ddcb9897604ab1f875
Stanislav Ursache <d34dkn16h7@gmail.com>
2015-01-24 15:32:26 +0200

    uber update!

--- Assets/Scripts/meteor.cs
+++ Assets/Scripts/meteor.cs
@@ -1,36 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class meteor : MonoBehaviour {
-	float position = 0;
-	public float speed = 5f;
-	public Transform[] parts;
-	Renderer[] placeholder;
+public class meteor : MonoBehaviour
+{
+    float position = 0;
+    public float speed = 5f;
+    public Transform[] parts;
+    Renderer[] placeholder;
 
-	// Use this for initialization
-	void Start () {
-		generator ();
-		placeholder = GetComponentsInChildren<Renderer> ();
-	}
+    // Use this for initialization
+    void Start()
+    {
+        generator();
+        placeholder = GetComponentsInChildren<Renderer>();
+    }
 
-	void generator(){
-		int partToRemove = Random.Range (0, parts.Length);
-		parts [partToRemove].gameObject.SetActive (false);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		position += Time.deltaTime/speed;
-		
-		transform.position = Vector3.Lerp(new Vector3(0,1,100), new Vector3(0,1,-10), position );
-		if (position>1) {
-			Destroy(gameObject);
-		}
-		if (position > 0.9f) {
-			foreach(var r in placeholder){
-				r.material.color= new Color(1,1,1,(1-position));
-			}
-		}
+    void generator()
+    {
+        int partToRemove = Random.Range(0, parts.Length);
+        parts[partToRemove].gameObject.SetActive(false);
+    }
 
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        position += Time.deltaTime / speed;
+
+        transform.position = Vector3.Lerp(new Vector3(0, 0, 100), new Vector3(0, 0, -10), position);
+        if (position > 1)
+        {
+            Destroy(gameObject);
+        }
+        if (position > 0.9f)
+        {
+            foreach (var r in placeholder)
+            {
+                var col = r.material.color;
+                var alpha = (1f - position) / .1f;
+                r.material.color = new Color(col.r, col.g, col.b, alpha);
+            }
+        }
+
+    }
 }
