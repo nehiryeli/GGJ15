284cb3cb8c00e56df70dcc5bd22c3dd8f4584646
Hakan Tatlı <hakantatli14@gmail.com>
2015-01-24 10:47:03 +0200

    Enemy spawner update

--- Assets/Scripts/enemySpawner.cs
+++ Assets/Scripts/enemySpawner.cs
@@ -4,26 +4,25 @@ using System.Collections;
 public class enemySpawner : MonoBehaviour {
 	public Transform meteor;
 	Transform meteorObj;
-	float position = 0;
+
 	public float spawnTime=3;
 	// Use this for initialization
 	IEnumerator Start () {
 		while (true) {
 			this.spawn ();
 			yield return new WaitForSeconds(spawnTime);
-			position=0;
+
 		}
 
 
 	}
 	public void spawn(){
-		meteorObj = (Transform)Instantiate(meteor, new Vector3(0, 0, 0), Quaternion.identity) as Transform;
+		meteorObj = (Transform)Instantiate(meteor) as Transform;
+		meteorObj.position = new Vector3 (500, 0, 0);
 	}
 	// Update is called once per frame
 	void Update () {
-		position += Time.deltaTime;
 
-		meteorObj.position = Vector3.Lerp(new Vector3(10,0,0), new Vector3(0,0,0), position );
 
 	}
 
--- Assets/meteor.cs
+++ Assets/meteor.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class meteor : MonoBehaviour {
 	public Transform T;
-
+	float position = 0;
+	public float speed = 5f;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,6 +12,11 @@ public class meteor : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		position += Time.deltaTime/speed;
+		
+		transform.position = Vector3.Lerp(new Vector3(0,1,100), new Vector3(0,1,-10), position );
+		if (position>1) {
+			Destroy(gameObject);		
+		}
 	}
 }
