c7bb0cbc822977da31de4220b64a3cb95a839ca6
Hakan Tatlı <hakantatli14@gmail.com>
2015-01-24 12:28:02 +0200

    Meteor variants

--- Assets/Scripts/meteor.cs
+++ Assets/Scripts/meteor.cs
@@ -2,12 +2,20 @@
 using System.Collections;
 
 public class meteor : MonoBehaviour {
-	public Transform T;
 	float position = 0;
 	public float speed = 5f;
+	public Transform[] parts;
+	Renderer[] placeholder;
+
 	// Use this for initialization
 	void Start () {
-	
+		generator ();
+		placeholder = GetComponentsInChildren<Renderer> ();
+	}
+
+	void generator(){
+		int partToRemove = Random.Range (0, parts.Length);
+		parts [partToRemove].gameObject.SetActive (false);
 	}
 	
 	// Update is called once per frame
@@ -16,7 +24,13 @@ public class meteor : MonoBehaviour {
 		
 		transform.position = Vector3.Lerp(new Vector3(0,1,100), new Vector3(0,1,-10), position );
 		if (position>1) {
-			Destroy(gameObject);		
+			Destroy(gameObject);
 		}
+		if (position > 0.9f) {
+			foreach(var r in placeholder){
+				r.material.color= new Color(1,1,1,(1-position));
+			}
+		}
+
 	}
 }
