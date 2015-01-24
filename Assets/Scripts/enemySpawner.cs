b976382c8ff0e766dc382722177b5126dd2898a7
Hakan Tatlı <hakantatli14@gmail.com>
2015-01-24 03:14:35 +0200

    Enemy Spawner Script init

---
+++ Assets/Scripts/enemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemySpawner : MonoBehaviour {
+	public Transform meteor;
+	Transform meteorObj;
+	float position = 0;
+	public float spawnTime=3;
+	// Use this for initialization
+	IEnumerator Start () {
+		while (true) {
+			this.spawn ();
+			yield return new WaitForSeconds(spawnTime);
+			position=0;
+		}
+
+
+	}
+	public void spawn(){
+		meteorObj = (Transform)Instantiate(meteor, new Vector3(0, 0, 0), Quaternion.identity) as Transform;
+	}
+	// Update is called once per frame
+	void Update () {
+		position += Time.deltaTime;
+
+		meteorObj.position = Vector3.Lerp(new Vector3(10,0,0), new Vector3(0,0,0), position );
+
+	}
+
+
+}
