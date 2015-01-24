ffeabe1c24604dd6d3d3f14d4a711ca8778f567f
utkuolcar <utkuolcar@gmail.com>
2015-01-24 10:48:20 +0200

    PlayerMovement

---
+++ Assets/Script/PlanetMovement.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+using System.Collections;
+public enum Pozisyon
+{
+	NW,
+	NE,
+	SW,
+	SE,
+}
+public enum Element{
+	Ateş,
+	Su,
+	Toprak,
+	Elektrik,
+}
+
+public class Player{
+	public Player (KeyCode gelenCekimTusu,KeyCode gelenElementTusu,Pozisyon gelenKose,Element gelenElement)
+		{
+		cekimTusu = gelenCekimTusu;
+		elementTusu = gelenElementTusu;
+		kose = gelenKose;
+		element = gelenElement;
+		cekimTusuBasildiMi = false;
+		}
+	public KeyCode cekimTusu {
+				get;
+				set;
+	}
+	public KeyCode elementTusu {
+				get;
+				set;
+	}
+	public Pozisyon kose {
+				get;
+				set;
+	}
+	public Element element {
+				get;
+				set;
+	}
+	public bool cekimTusuBasildiMi {
+				get;
+				set;
+	}
+	public bool elementTusuBasildiMi {
+		get;
+		set;
+	}
+
+}
+public class PlanetMovement : MonoBehaviour {
+	Player[] playerArray ;
+	const int  maxPosition = 20;
+	Vector3 target ;
+	public float Hiz = 100;
+	Vector3 direction;
+	float oldTime;
+	// Use this for initialization
+	void Start () {
+		playerArray = new Player[] {
+			new Player (KeyCode.A, KeyCode.Z, Pozisyon.NW,Element.Ateş),
+			new Player (KeyCode.G, KeyCode.B, Pozisyon.NE,Element.Elektrik),
+			new Player (KeyCode.UpArrow, KeyCode.DownArrow, Pozisyon.SW,Element.Su),
+			new Player (KeyCode.Keypad6, KeyCode.Keypad3, Pozisyon.SE,Element.Toprak)	
+		};
+		target = new Vector3 (0, 0, transform.position.z);
+		transform.position = new Vector3 (0, 0, transform.position.z);
+		direction = new Vector3 (0, 0, transform.position.z);
+		oldTime = Time.time;
+	}
+	
+	// Update is called once per frame
+
+	void Update () {
+				foreach (var item in playerArray) {
+						if (Input.GetKeyDown (item.cekimTusu)) {
+				item.cekimTusuBasildiMi = true;
+						}
+						if (Input.GetKeyUp (item.cekimTusu)) {
+				item.cekimTusuBasildiMi = false;
+						}
+			if (Input.GetKeyDown (item.elementTusu)) {
+				item.elementTusuBasildiMi = true;
+			}
+			if (Input.GetKeyUp (item.elementTusu)) {
+				item.elementTusuBasildiMi = false;
+			}
+		}
+		target = new Vector3(0,0,transform.position.z);
+		TargetHesapla ();
+
+		if ((target - transform.position).magnitude != 0) {
+			direction = (target - transform.position) / (target - transform.position).magnitude;
+				}
+
+		HareketEttir (direction);
+		oldTime = Time.time;
+		}
+
+	void TargetHesapla(){
+		foreach (var item in playerArray) {
+					if(item.cekimTusuBasildiMi){
+				Vector3 targetFaktor = PlayerPozisyonuIleTargetBelirle(item.kose);
+				target.x +=  targetFaktor.x;
+				target.y += targetFaktor.y;
+			}
+		}
+		if (playerArray[0].cekimTusuBasildiMi == false && playerArray[1].cekimTusuBasildiMi == false && playerArray[2].cekimTusuBasildiMi == false && playerArray[3].cekimTusuBasildiMi == false) {
+			target = new Vector3(0,0,transform.position.z);
+		}
+		else{
+		if (target.x > maxPosition)
+			target.x = maxPosition;
+		if (target.y > maxPosition)
+			target.y = maxPosition;
+		if (target.x < (-1)* maxPosition)
+			target.x = (-1)*maxPosition;
+		if (target.y < (-1)*maxPosition)
+			target.y =(-1)* maxPosition;
+		}
+		}
+
+	Vector3 PlayerPozisyonuIleTargetBelirle(Pozisyon pozisyon){
+				switch (pozisyon) {
+				case Pozisyon.NE:		
+			return new Vector3(maxPosition,maxPosition,transform.position.z);
+				case Pozisyon.NW:		
+			return new Vector3((-1)*maxPosition,maxPosition,transform.position.z);
+				case Pozisyon.SE:
+			return new Vector3(maxPosition,(-1)*maxPosition,transform.position.z);
+				case Pozisyon.SW:
+			return new Vector3((-1)*maxPosition,(-1)*maxPosition,transform.position.z);
+				default:
+			return new Vector3(0,0,transform.position.z);
+				}
+		}
+
+	void HareketEttir(Vector3 dir){
+		if ((target - transform.position).magnitude < (dir * ((Time.time - oldTime) * Hiz)).magnitude)
+						transform.position = target;
+				else {
+			transform.Translate (dir *((Time.time - oldTime) * Hiz));		
+				}
+
+	}
+}
