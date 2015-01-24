79fdf3785f64135e3481bc8849980ae61580d73d
Erdin Kacan <erdin.kacan@yahoo.com.tr>
2015-01-24 01:07:46 +0000

    Ui update.

--- Assets/Scripts/GuiManager.cs
+++ Assets/Scripts/GuiManager.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 public class GuiManager : MonoBehaviour
 {
     public Text scoreText;
-    public GameObject lifePrefab;
     public float lifePadding = 50;
     public Transform livesParent;
     GameObject[] _lives;
@@ -15,39 +14,7 @@ public class GuiManager : MonoBehaviour
     void Start()
     {
         _transform = transform;
-
-        int _lifeCount = 3;
-        GameObject[] _lives = new GameObject[_lifeCount];
-        for (int i = 0; i < _lifeCount; i++)
-        {
-            _lives[i] = Instantiate(lifePrefab) as GameObject;
-            _lives[i].transform.SetParent(livesParent, false);
-            _lives[i].GetComponent<RectTransform>().localScale = _lives[i].GetComponent<RectTransform>().localScale + Vector3.one * lifePadding * i;
-        }
-
-        #region ImageBasedLives
-        //int _lifeCount = 3;
-        //float _firstLifePosition;
-        //if (_lifeCount % 2 != 0)
-        //{
-        //    _firstLifePosition = -lifePadding * ((_lifeCount - 1) * 0.5f);
-        //}
-        //else
-        //{
-        //    _firstLifePosition = (-lifePadding * ((_lifeCount - 1) * 0.5f)) + (lifePrefab.GetComponent<RectTransform>().rect.size.x * 0.25f);
-        //}
-        //_lives = new GameObject[_lifeCount];
-
-        //for (int i = 0; i < _lifeCount; i++)
-        //{
-        //    _lives[i] = Instantiate(lifePrefab) as GameObject;
-        //    _lives[i].transform.SetParent(livesParent, false);
-        //    Vector2 _newPosition = _lives[i].GetComponent<RectTransform>().anchoredPosition;
-        //    _lives[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(_firstLifePosition + (i * lifePadding), _newPosition.y);
-        //} 
-        #endregion
     }
-
     void OnScoreUpdated(int score)
     {
         scoreText.text = score.ToString("N0");
@@ -57,17 +24,18 @@ public class GuiManager : MonoBehaviour
     {
         for (int i = 0; i < livesParent.childCount; i++)
         {
-            if (i > livesLeft - 1) livesParent.GetChild(i).gameObject.SetActive(false);
-            else livesParent.GetChild(i).gameObject.SetActive(true);
+            if (i > livesLeft - 1)
+            {
+                livesParent.GetChild(i).GetComponent<Animator>().SetTrigger("LifeLost");
+            }
         } 
     }
 
     void Update()
     {
-        //Test
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            OnLifeChanged(1);
+            OnLifeChanged(0);
         }
     }
 }
