dfcd2f72439d6bf41d21eba9b7b6163f5b4da21c
Stanislav Ursache <d34dkn16h7@gmail.com>
2015-01-25 01:26:25 +0200

    dd

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -34,10 +34,10 @@ public class GameManager : MonoBehaviour
     {
         players = new List<Player>
             {
-			    new Player (KeyCode.A, KeyCode.Z, Pozisyon.NW,Element.Ateş),
-			    new Player (KeyCode.G, KeyCode.B, Pozisyon.NE,Element.Elektrik),
-			    new Player (KeyCode.UpArrow, KeyCode.DownArrow, Pozisyon.SE,Element.Su),
-			    new Player (KeyCode.Keypad6, KeyCode.Keypad3, Pozisyon.SW,Element.Toprak)
+			    new Player (KeyCode.A, KeyCode.Z, Pozisyon.NW,Element.Toprak),
+			    new Player (KeyCode.G, KeyCode.B, Pozisyon.NE,Element.Su),
+			    new Player (KeyCode.UpArrow, KeyCode.DownArrow, Pozisyon.SE,Element.Elektrik),
+			    new Player (KeyCode.Keypad6, KeyCode.Keypad3, Pozisyon.SW,Element.Ateş)
             };
     }
 
