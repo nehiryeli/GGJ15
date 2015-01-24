9b01905a9a80b7bc1bea2f7dc1923c3ace6ba3f2
Stanislav Ursache <d34dkn16h7@gmail.com>
2015-01-24 02:29:58 +0200

    game_manager
    init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager inst;
+
+    List<Player> players;
+
+    public static Action
+        a_onGameStart,
+        a_onGameEnd;
+
+    public static Action<int>
+        onScoreUpdated,
+        onLifeChanged;
+    //a_onPowerupUpdate
+
+    void Start()
+    {
+        players = new List<Player>
+            {
+                 new Player()
+                ,new Player()
+                ,new Player()
+                ,new Player()
+            };
+    }
+
+    void SwitchPlayers(Direction d)
+    {
+
+    }
+
+    void Awake()
+    { inst = this; }
+}
+
+public class Player
+{
+
+}
+
+public enum Direction
+{
+    Left,
+    Right
+}
