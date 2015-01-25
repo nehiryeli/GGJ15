71a26ee01d38aa67d7b169e3aa3718c2f6282183
Stanislav Ursache <d34dkn16h7@gmail.com>
2015-01-25 10:36:56 +0200

    bg mv

--- Assets/bgRot.cs
+++ Assets/bgRot.cs
@@ -1,15 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class bgRot : MonoBehaviour {
+public class bgRot : MonoBehaviour
+{
+    public float lim;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    Transform t;
+
+    IEnumerator Start()
+    {
+        t = transform;
+
+        var p = 0f;
+
+        var f_pos = t.position;
+        var t_pos = getVec3();
+
+        while (true)
+        {
+            p += Time.deltaTime;
+
+            t.position = Vector3.Lerp(f_pos, t_pos,p);
+
+            yield return new WaitForFixedUpdate();
+
+            if(p > 1)
+            {
+                f_pos = t_pos;
+                t_pos = getVec3();
+                p = 0f;
+            }
+        }
+    }
+
+    Vector3 getVec3()
+    {
+        var r1 = Random.Range(-lim, lim);
+        var r2 = Random.Range(-lim, lim);
+
+        return new Vector3(r1, r2, t.position.z);
+    }
+
 }
