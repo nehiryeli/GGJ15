460c54d8d091143be11033a8ef7d7972524ba5de
Hakan Tatlı <hakantatli14@gmail.com>
2015-01-25 15:47:32 +0200

    planet change element sounds

--- Assets/elenemtalEff.cs
+++ Assets/elenemtalEff.cs
@@ -23,7 +23,11 @@ public class elenemtalEff : MonoBehaviour
         a_water,
         a_earth,
         a_elect,
-        a_ch;
+        a_ch,
+		g_fire,
+		g_water,
+		g_lightning,
+		g_earth;
 
 
     public Renderer target_r;
@@ -55,11 +59,12 @@ public class elenemtalEff : MonoBehaviour
 
     public void Effect()
     {
-        au_src.PlayOneShot(a_ch);
+        //au_src.PlayOneShot(a_ch);
         switch (GameManager.inst.pMove.status)
         {
             case Element.Ateş:
                 target_r.material = m_fire;
+				au_src.PlayOneShot(g_fire);
                 fire.SetActive(true);
                 water.SetActive(false);
                 earth.SetActive(false);
@@ -67,6 +72,7 @@ public class elenemtalEff : MonoBehaviour
                 break;
             case Element.Su:
                 target_r.material = m_water;
+				au_src.PlayOneShot(g_water);
                 fire.SetActive(false);
                 water.SetActive(true);
                 earth.SetActive(false);
@@ -74,6 +80,7 @@ public class elenemtalEff : MonoBehaviour
                 break;
             case Element.Toprak:
                 target_r.material = m_earth;
+				au_src.PlayOneShot(g_earth);
                 fire.SetActive(false);
                 water.SetActive(false);
                 earth.SetActive(true);
@@ -81,6 +88,7 @@ public class elenemtalEff : MonoBehaviour
                 break;
             case Element.Elektrik:
                 target_r.material = m_elect;
+				au_src.PlayOneShot(g_lightning);
                 fire.SetActive(false);
                 water.SetActive(false);
                 earth.SetActive(false);
