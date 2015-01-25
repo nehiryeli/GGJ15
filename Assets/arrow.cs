1dda496fc865276f62b8a2def991c247145c738b
Stanislav Ursache <d34dkn16h7@gmail.com>
2015-01-25 12:50:20 +0200

    asd

--- Assets/arrow.cs
+++ Assets/arrow.cs
@@ -5,8 +5,11 @@ public class arrow : MonoBehaviour
 {
     public int pID;
 
+    Renderer rn;
+
     void Awake()
     {
+        rn = renderer;
         GameManager.a_onInversTrigger += Rot_Arrow;
     }
 
@@ -16,7 +19,10 @@ public class arrow : MonoBehaviour
     }
 
     void Update()
-    { }
+    {
+        var pl = GameManager.getPlayers();
+        renderer.material.color = pl[pID].cekimTusuBasildiMi ? Color.red : Color.green;
+    }
 
     void OnDestroy()
     {
